76914cf80db001c946976cc7dbea30bc0296b708
Brian Tang <ashrielbrian@gmail.com>
2022-04-27 23:34:16 +0800

    Adds UserNotFoundException

---
+++ src/main/java/com/moneylion/featureswitch/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.moneylion.featureswitch.exceptions;
+
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message) { super(message); }
+
+}
